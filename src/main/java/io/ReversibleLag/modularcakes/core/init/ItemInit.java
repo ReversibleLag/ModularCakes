9ed2f136399c5fe61a9fc225a217178e54b61dba
ReversibleLag <kaisouth123@gmail.com>
2021-05-10 17:08:29 -0400

    added butter
    added butter icon as well as added creative tab for all the items

--- src/main/java/io/ReversibleLag/modularcakes/core/init/ItemInit.java
+++ src/main/java/io/ReversibleLag/modularcakes/core/init/ItemInit.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package io.ReversibleLag.modularcakes.core.init;
 
 import io.ReversibleLag.modularcakes.ModularCakes;
@@ -18,3 +19,26 @@ public class ItemInit {
 	public static final RegistryObject<Item> SALT = ITEMS.register("salt", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_FOOD)));
 
 }
+=======
+package io.ReversibleLag.modularcakes.core.init;
+
+import io.ReversibleLag.modularcakes.ModularCakes;
+import io.ReversibleLag.modularcakes.ModularCakes.ModularCakesTab;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ItemInit {
+	
+	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, 
+			ModularCakes.MOD_ID);
+
+	public static final RegistryObject<Item> FLOUR = ITEMS.register("flour", () -> new Item(new Item.Properties().tab(ModularCakes.MODULAR_CAKE_TAB)));
+	
+	public static final RegistryObject<Item> BUTTER = ITEMS.register("butter", () -> new Item(new Item.Properties().tab(ModularCakes.MODULAR_CAKE_TAB)));
+
+	
+}
+>>>>>>> Stashed changes
